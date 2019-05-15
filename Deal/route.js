d38c8ec3d671b335df50284d29370fad41d343f0
salim aouali <salimaouali08@gmail.com>
2019-05-15 12:03:14 +0200

    différentes modifs

--- Deal/route.js
+++ Deal/route.js
@@ -4,24 +4,36 @@ app.use(bodyParser.json());
 const action = require('./action');
 
 
-app.post ('/create', function(req, res){
+app.post('/create', function (req, res) {
     action.creerDeal(req, res);
 })
 
-app.get('/', function(req, res){
-    action.afficherLesDeal(req,res);
+app.get('/', function (req, res) {
+    action.afficherLesDeal(req, res);
+})
+app.put('/update/:id', function (req, res) {
+    action.miseAjourDeal(req, res);
+});
+app.delete('/supp/:id', function (req, res) {
+    action.supprimerDeal(req, res);
+});
+app.get('/:id', function (req, res) {
+    action.afficherDeal(req, res);
 })
 
-app.get('/:id', function(req,res){
-    action.afficherDeal(req,res);
+app.get('/up/:id', function (req, res) {
+    action.voterPlus(req, res);
 })
 
-app.get('/up/:id', function(req,res){
-    action.voterPlus(req,res);
+app.get('/down/:id', function (req, res) {
+    action.voterMoins(req, res);
 })
 
-app.get('/down/:id', function(req,res){
-    action.voterMoins(req,res);
+// app.get('/userdeal', function (req, res) {
+//     action.afficherDealsUser(req, res);
+// })
+app.get('/userdeals/:username', function (req, res) {
+    action.afficherDealsUser(req, res);
 })
 
 
